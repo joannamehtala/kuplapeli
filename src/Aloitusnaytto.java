f6be4ebd28df364fc8009c9dd916a22d2ddf5dd9
Joanna <joanna_mehtala@hotmail.com>
2013-01-03 00:30:04 +0200

    Aloitusnäytön luokka kommentoitu.

--- src/Aloitusnaytto.java
+++ src/Aloitusnaytto.java
@@ -4,9 +4,6 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Toolkit;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -14,9 +11,21 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
-
+/**
+ * Aloitusnäytön luokka. Aloitusnäytöllä on taustakuva ja nappulat, joita
+ * painamalla voi aloittaa pelin, saada ohjeet näkyviin tai lopettaa ohjelman
+ * suorituksen.
+ * @author Joanna
+ *
+ */
+@SuppressWarnings("serial")
 public class Aloitusnaytto extends JPanel {
 
+	/**
+	 * Attribuuteiksi asetetaan aloitusnäytön taustakuva sekä aloita- ja 
+	 * ohjenappulan kuvat. Lisäksi asetetaan attribuuteikse itse nappulat,
+	 * ikkuna, nappuloiden reunukset sekä insetsit.
+	 */
 	private static final Image kuplapeli =
 			Toolkit.getDefaultToolkit().createImage("media/kuplapeli.png");
 	private static final ImageIcon aloita_normal = 
@@ -28,6 +37,13 @@ public class Aloitusnaytto extends JPanel {
 	private Border reunus = BorderFactory.createEmptyBorder();
 	private Insets insets;
 
+	/**
+	 * Konstruktorissa alustetaan attribuutteja ja asetetaan paneelille koko.
+	 * LayoutManager on null koska näin voidaan asettaa nappuloille paikat
+	 * pikseleiden avulla. Lisäksi määritellään nappuloiden ominaisuudet ja
+	 * lisätään ne paneeliin.
+	 * @param ikkuna
+	 */
 	public Aloitusnaytto(Ikkuna ikkuna){
 		this.ikkuna = ikkuna;
 		this.setPreferredSize(new Dimension(500, 600));
@@ -35,41 +51,58 @@ public class Aloitusnaytto extends JPanel {
 		this.insets = this.getInsets();
 
 		/*
-		 * Aloitusnappi
+		 * Aloitusnapin ominaisuudet.
 		 */
 		this.aloitus = new JButton();
 		this.aloitus.setIcon(aloita_normal);
 		this.aloitus.setPreferredSize(new Dimension(81, 39));
 		this.aloitus.setBorder(reunus);
-		this.aloitus.addMouseListener(new Hiirikuuntelija_Aloita(this, this.ikkuna));
+		this.aloitus.addMouseListener(new Hiirikuuntelija_Aloita(this, 
+				this.ikkuna));
 		this.add(this.aloitus);
 		this.aloitus.setBounds(120 + this.insets.left,145 + this.insets.top, 
 				81, 39);
 
 		/*
-		 * Ohjenappi
+		 * Ohjenapin ominaisuudet.
 		 */
 		this.ohjeet = new JButton();
 		this.ohjeet.setIcon(ohjeet_normal);
 		this.ohjeet.setPreferredSize(new Dimension(93, 37));
 		this.ohjeet.setBorder(reunus);
-		this.ohjeet.addMouseListener(new Hiirikuuntelija_Ohjeet(this, this.ikkuna));
+		this.ohjeet.addMouseListener(new Hiirikuuntelija_Ohjeet(this, 
+				this.ikkuna));
 		this.add(this.ohjeet);
 		this.ohjeet.setBounds(205 + this.insets.left, 170 + this.insets.top, 
 				93, 37);
 
 	}
 
+	/**
+	 * Metodilla piirretään taustakuva.
+	 */
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D)g;
 		g2d.drawImage(kuplapeli, 0, 0, this);
 	}
 	
+	/**
+	 * Metodilla voi asettaa aloitusnapille kuvan. Tätä tarvitaan, jotta
+	 * hiirikuntelijaluokassa voidaan asettaa napille uusi kuva, kun hiirtä
+	 * liikutetaan kuvan päälle.
+	 * @param i
+	 */
 	public void aloita_asetaKuva(ImageIcon i){
 		this.aloitus.setIcon(i);
 	}
 	
+	/**
+	 * Metodilla voi asettaa ohjenapille kuvan. Tätä tarvitaan, jotta
+	 * hiirikuntelijaluokassa voidaan asettaa napille uusi kuva, kun hiirtä
+	 * liikutetaan kuvan päälle.
+	 * @param i
+	 */
 	public void ohjeet_asetaKuva(ImageIcon i){
 		this.ohjeet.setIcon(i);
 	}
