aca05906cd3067313ace9b5a315e24eccf19651b
Joanna <joanna_mehtala@hotmail.com>
2013-01-14 13:48:11 +0200

    Kommenoitu kuplaaaaa

--- src/Kupla.java
+++ src/Kupla.java
@@ -19,13 +19,27 @@ import java.util.Set;
  *
  */
 public class Kupla {
+	
+	/** Kuplan s‰de. */
 	private double sade;
+	
+	/** Kuplan v‰ri. */
 	private Vari vari;
+	
+	/** Kertoo, onko kupla ehj‰. */
 	private boolean ehja;
+	
+	/** Random arpomista varten. */
 	public static final Random rand = new Random();
+	
+	/** Kuplan x- ja y-koordinaatit. */
 	private double x;
 	private double y;
+	
+	/** Maailma, jossa kupla sijaitsee. */
 	private Maailma maailma;
+	
+	/** Kuplien kuvat. */
 	private static final Image punainen = 
 			Toolkit.getDefaultToolkit().createImage("media/punainenkupla.png");
 	private static final Image sininen =
@@ -38,13 +52,12 @@ public class Kupla {
 			Toolkit.getDefaultToolkit().createImage("media/keltainenkupla.png");
 
 	/**
-	 * Kuplan konstruktori. Kaikki kuplat ovat samankokokisia, joten s‰de
-	 * alustetaan. Kuplalle annetaan myˆs v‰ri ja se saa tiedon siit‰, ett‰
-	 * se on ehj‰. Jokainen kupla on luotaessa aktiivinen, sill‰ se on t‰llˆin
-	 * seuraavaksi ammuttavana.
-	 * @param vari
+	 * Kuplan konstruktori. Alustetaan attribuutit; jokainen kupla on aluksi
+	 * ehj‰ ja sille arvotaan v‰ri.
+	 * @param x-koordinaatti, y-koordinaatti, kuplan maailma
 	 */
 	public Kupla(double x, double y, Maailma maailma){
+		
 		this.ehja = true;
 		this.maailma = maailma;
 		this.x = x;
@@ -66,7 +79,7 @@ public class Kupla {
 	}
 
 	/**
-	 * Palauttaa kuplan nykyisen x-koordinaatin.
+	 * Palauttaa kuplan nykyisen x-koordinaatin (vasen yl‰kulma).
 	 * @return x-koord
 	 */
 	public double annaX(){
@@ -74,7 +87,7 @@ public class Kupla {
 	}
 
 	/**
-	 * Palauttaa kuplan nykyisen y-koordinaatin.
+	 * Palauttaa kuplan nykyisen y-koordinaatin (vasen yl‰kulma).
 	 * @return y-koord
 	 */
 	public double annaY(){
@@ -115,10 +128,17 @@ public class Kupla {
 		return this.sade;
 	}
 
+	/**
+	 * Poksautetaan kupla asettamalla sen attribuutti "ehja" falseksi.
+	 */
 	public void poksahda(){
 		this.ehja = false;
 	}
 
+	/**
+	 * Palauttaa kuplan kuvan v‰rist‰ riippuen.
+	 * @return kuplan kuva
+	 */
 	public Image annaKuva(){
 		switch (this.vari){
 		case PUNAINEN:
@@ -135,26 +155,47 @@ public class Kupla {
 		return null;
 	}
 
+	/**
+	 * Asetetaan kuplalle uusi sijainti.
+	 * @param x, x-koordinaatti
+	 * @param y, y-koordinaatti
+	 */
 	protected void asetaSijainti(double x, double y){
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * Testaamista helpottamaan toStringin ylikirjoitus.
+	 */
 	public String toString(){
 		return "Kuplan v‰ri on " + this.annaVari();
 	}
 
+	/**
+	 * Palauttaa listan kuplan naapureista.
+	 * @return naapurit
+	 */
 	private ArrayList<Kupla> annaNaapurit(){
+		
 		ArrayList<Kupla> naapurit = new ArrayList<Kupla>();
+		
+		//Tarkastelu tehd‰‰n vain, jos maailma ei ole null.
 		if (!(this.maailma == null)){
 			
+			//Superkuplalle asetetaan isompi s‰de, jotta sen avulla saadaan
+			//helposti poksautettua enemm‰n kuplia.
 			if (this instanceof Superkupla){
-				this.sade = 40;
+				this.sade = 48;
 			}
 
+			//K‰yd‰‰n l‰pi kaikki maailman kuplat.
 			for (int i = 0; i < this.maailma.annaKuplat().size(); i++){
 				Kupla tutkittava = this.maailma.annaKuplat().get(i);
 
+				//Jos kuplien keskipisteiden v‰linen et‰isyys on yht‰ kuin
+				//halkaisija (plus 6 jotta aina toimii), niin lis‰t‰‰n kupla
+				//naapureihin.
 				if (Point2D.distance(this.annaKeskiX(), this.annaKeskiY(), 
 						tutkittava.annaKeskiX(), tutkittava.annaKeskiY()) 
 						<= 2 * this.sade + 6 && tutkittava.onEhja()) {
@@ -165,6 +206,12 @@ public class Kupla {
 		return naapurit;
 	}
 
+	/**
+	 * Metodi palauttaa boolean-arvon true, jos parametrina annettu kupla
+	 * on samanv‰rinen, ja false, jos se on eri v‰rinen.
+	 * @param Kupla toinen
+	 * @return true, jos samanv‰rinen, false, jos eri v‰rinen
+	 */
 	public boolean onSamanVarinen(Kupla toinen){
 		if (this.annaVari() == toinen.annaVari()){
 			return true;
@@ -172,6 +219,12 @@ public class Kupla {
 		return false;
 	}
 
+	/**
+	 * Metodi tarkistaa, mitk‰ kaikki kuplat on poksautettava tˆrm‰yksen
+	 * tapahtuessa. Kaikki samanv‰riset kuplat on poksautettava, ja superkuplan
+	 * tapauksessa poksautetaan naapureita suuremmalla alueella kuin muiden
+	 * kuplien tapauksessa.
+	 */
 	public void tarkistaPoksautettavat(){
 
 		//Alustetaan tarkisteltavien kuplien lista kuplan v‰littˆm‰ss‰
@@ -181,26 +234,32 @@ public class Kupla {
 		HashSet<Kupla> tarkastetut = new HashSet<Kupla>();
 		tarkastetut.add(this);
 
+		//Superkupla poksauttaa kaikki naapurinsa.
 		if (this instanceof Superkupla){
 			poksautettavat = this.annaNaapurit();
 
 		} else {
 
-			//K‰yd‰‰n l‰pi samanv‰riset kuplat, joita tulee lis‰‰ kun silmukkaa
-			//k‰yd‰‰n l‰pi
+			//K‰yd‰‰n l‰pi poksautettavat kuplat, joita tulee lis‰‰ kun
+			//silmukkaa k‰yd‰‰n l‰pi
 			for (int i = 0; i < poksautettavat.size(); i++){
 				Kupla t = poksautettavat.get(i);
 				ArrayList<Kupla> tempNaapurit = t.annaNaapurit();
+				
 				//K‰yd‰‰n l‰pi ‰sken listatut kuplan t naapurit yksi kerrallaan,
 				//ja jokaisen kohdalla tarkistetaan, onko sit‰ tutkittu aiemmin
 				//ja jos ei, se lis‰t‰‰n tutkittujen listalle ja tarkistetaan,
 				//onko se samanv‰rinen kuin ammuttu kupla.
 				for (Kupla n : tempNaapurit){
+					
+					//Kupla tarkastetaan vain, jos sit‰ ei viel‰ ole tarkastettu
+					//(v‰ltet‰‰n StackOverFlowError).
 					if (!tarkastetut.contains(n)){
 						tarkastetut.add(n);
+						
 						if (this.onSamanVarinen(n)){
-							//Lis‰t‰‰n samanv‰risiin n, jolle tehd‰‰n uudestaan
-							//koko tarkastelu silmukan alusta l‰htien.
+							//Lis‰t‰‰n poksautettaviin n, jolle tehd‰‰n 
+							//uudestaan koko tarkastelu silmukan alusta l‰htien.
 							poksautettavat.add(n);
 						}
 					}
@@ -208,23 +267,24 @@ public class Kupla {
 			}
 		}
 
-		//Nyt samanv‰riset sis‰lt‰‰ kaikki ne samanv‰riset kuplat, joihin
+		//Nyt poksautettavat sis‰lt‰‰ kaikki ne samanv‰riset kuplat, joihin
 		//kupla ja sen naapurit ja naapurin naapurit jne. koskevat.
 
-		//Poks!!!
-
+		//Poksautetaan kuplat :)
 		if (!(this instanceof Superkupla)){
+			
+			//Kuplat poksautetaan vain, jos poksautettavia on 3 tai enemm‰n.
 			if (poksautettavat.size() >= 3){
 				for (int i = 0; i < poksautettavat.size(); i++){
 					poksautettavat.get(i).poksahda();
 				}
 			}
+			
+		//Superkuplan tapauksessa poksautetaan kuplia aina.
 		} else {
 			for (int i = 0; i < poksautettavat.size(); i++){
 				poksautettavat.get(i).poksahda();
 			}
 		}
-
-
 	}
 }
